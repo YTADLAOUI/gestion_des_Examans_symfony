e04af6e49b1bce0aff51fc19b4727d368967975c
youssef tadlaoui <usftadlaoui@gmail.com>
2023-05-09 00:21:54 +0100

    factory

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,13 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Factory\EnseignantFactory;
+use App\Factory\EtudiantFactory;
+use App\Factory\FiliereFactory;
+use App\Factory\ModuleFactory;
+use App\Factory\NoteFactory;
+use App\Factory\SemesterFactory;
+use App\Factory\UserFactory;
 
 class AppFixtures extends Fixture
 {
@@ -11,7 +18,13 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
-
+        FiliereFactory::createMany(10);
+        EnseignantFactory::createMany(10);
+        EtudiantFactory::createMany(10);
+        SemesterFactory::createMany(6);
+        ModuleFactory::createMany(23);
+        NoteFactory::createMany(10);
+        UserFactory::createMany(10);
         $manager->flush();
     }
 }
