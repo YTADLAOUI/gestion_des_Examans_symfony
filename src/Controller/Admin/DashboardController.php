aab5c7d8eef78e6059e2a95ee3f76358e1663ffe
youssef tadlaoui <usftadlaoui@gmail.com>
2023-05-09 02:00:10 +0100

    configMenu

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -45,6 +46,21 @@ class DashboardController extends AbstractDashboardController
     {
         return Dashboard::new()
             ->setTitle('GestionExmans');
+    }
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        return parent::configureUserMenu($user)
+            ->setName($user->getUserIdentifier())
+            ->setGravatarEmail($user->getEmail())
+         //   ->setAvatarUrl('https://www.clipartmax.com/png/full/405-4050774_avatar-icon-flat-icon-shop-download-free-icons-for-avatar-icon-flat.png')
+            ->displayUserAvatar(true);
+    }
+
+
+
+    public function configureAssets(): Assets
+    {
+        return Assets::new()->addCssFile('build/css/admin.css');
     }
 
     public function configureMenuItems(): iterable
